30612399c0f8c795797b47ac44c807354a2f7083
youngprinnce <ajiboyeadedotun16@gmail.com>
2023-06-29 01:22:42 +0100

    local and jwt strategies

---
+++ apps/auth/src/strategies/local.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { UsersService } from '../users/users.service';
+import { UserDocument } from '../users/models/user.schema';
+
+@Injectable()
+export class LocalStategy extends PassportStrategy(Strategy) {
+  constructor(private readonly usersService: UsersService) {
+    super({ usernameField: 'email' });
+  }
+
+  async validate(email: string, password: string): Promise<UserDocument> {
+    try {
+      return await this.usersService.verifyUser(email, password);
+    } catch (err) {
+      throw new UnauthorizedException(err);
+    }
+  }
+}
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -8,6 +8,8 @@ import {
   ReservationSchema,
 } from './models/reservation.schema';
 import { LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
@@ -16,6 +18,14 @@ import { LoggerModule } from '@app/common';
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+      envFilePath: './apps/reservations/.env',
+    }),
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
